ca45af5f317503657db52fe575953019e534fcd0
Pigeon0v0 <60414767+Pigeon0v0@users.noreply.github.com>
2025-08-09 19:38:28 +0800

    fix(text): 忘记改上游版本号了

--- Plain Craft Launcher 2/Modules/Base/ModBase.vb
+++ Plain Craft Launcher 2/Modules/Base/ModBase.vb
@@ -19,7 +19,7 @@ Public Module ModBase
     Public Const VersionStandardCode As String = "2.12.2." & VersionBranchCode
     Public Const CommitHash As String = "native" 'Commit Hash，由 GitHub Workflow 自动替换
     Public CommitHashShort As String = If(CommitHash = "native", "native", CommitHash.Substring(0, 7)) 'Commit Hash，取前 7 位
-    Public Const UpstreamVersion As String = "2.10.3" '上游版本
+    Public Const UpstreamVersion As String = "2.10.5" '上游版本
     Public Const VersionCode As Integer = 399 '内部版本号
     '自动生成的版本信息
 #If DEBUG Then
