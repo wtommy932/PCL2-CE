d66a7292005e53c2f1270798eee978289bef2b21
Pigeon0v0 <60414767+Pigeon0v0@users.noreply.github.com>
2025-08-10 12:18:54 +0800

    imp(path): 检测社交软件下载目录并给予警告 (#1329)

--- Plain Craft Launcher 2/Application.xaml.vb
+++ Plain Craft Launcher 2/Application.xaml.vb
@@ -129,6 +129,9 @@ WaitRetry:
             If Path.Contains(IO.Path.GetTempPath()) OrElse Path.Contains("AppData\Local\Temp\") Then
                 MyMsgBox("请将 PCL 从压缩包中解压之后再使用！" & vbCrLf & "在当前环境下运行可能会导致丢失游戏存档或设置，部分功能也可能无法使用！", "环境警告", "我知道了", IsWarn:=True)
             End If
+            If Path.ContainsF("wechat_files", True) OrElse Path.ContainsF("WeChat Files", True) OrElse Path.ContainsF("Tencent Files", True) Then
+                MyMsgBox("请不要将 PCL 放置在 QQ、微信、TIM 等社交软件的下载目录！" & vbCrLf & "在这些地方运行可能会导致丢失游戏存档或设置，部分功能也可能无法使用！", "环境警告", "我知道了", IsWarn:=True)
+            End If
             If Is32BitSystem Then
                 MyMsgBox("PCL 和新版 Minecraft 均不再支持 32 位系统，部分功能将无法使用。" & vbCrLf & "非常建议重装为 64 位系统后再进行游戏！", "环境警告", "我知道了", IsWarn:=True)
             End If
