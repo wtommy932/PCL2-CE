6ea817076552cd755e3199e7b28161e9603e7f4d
Open-KFC <168712411+Open-KFC@users.noreply.github.com>
2025-08-13 10:47:21 +0800

    refactor(setup): 迁移 SystemSystemUpdateBranch (#1349)

--- Plain Craft Launcher 2/Pages/PageSetup/ModSetup.vb
+++ Plain Craft Launcher 2/Pages/PageSetup/ModSetup.vb
@@ -6,7 +6,6 @@ Public Class ModSetup
 #Region "基础"
 
     Public Sub New()
-        SetupEntries.EntryDictionary("SystemSystemUpdateBranch") = New SetupEntry(SetupEntrySource.PathLocal, "SystemSystemUpdateBranch", If(VersionBaseName.Contains("beta"), 1, 0))
         AddHandler SetupService.SetupChanged, AddressOf OnSetupChanged
     End Sub
 
