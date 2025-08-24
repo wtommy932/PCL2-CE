f5b991e5f2e7f15415c13f45584f5ed275807ae9
吴桐 <162002839+WT2024@users.noreply.github.com>
2025-08-24 14:32:44 +0800

    fix(authlib): 修复一些Bug (#1423)
    * fix(authlib): 修复一些Bug
    
    * refactor(authlib): 干脆重写得了
    
    ---------
    
    Co-authored-by: 任天天 <ruattqwq@outlook.com>

--- Plain Craft Launcher 2/Pages/PageLaunch/PageLoginAuth.xaml.vb
+++ Plain Craft Launcher 2/Pages/PageLaunch/PageLoginAuth.xaml.vb
@@ -1,9 +1,13 @@
-﻿Imports PCL.Core.Minecraft.Yggdrasil
+﻿Imports System.Net.Http
+Imports System.Text.RegularExpressions
+Imports PCL.Core.Minecraft.Yggdrasil
+Imports PCL.Core.Net
 
 Public Class PageLoginAuth
     Public Shared DraggedAuthServer As String = Nothing
     Private Sub Reload() Handles Me.Loaded
         Dim serverItems = TextServer.Items
+        serverItems.Clear()
         For Each serverName In PredefinedAuthServers.Keys
             serverItems.Add(New MyComboBoxItem() With {.Content = serverName})
         Next
@@ -13,88 +17,83 @@ Public Class PageLoginAuth
         End If
     End Sub
     Private Sub BtnBack_Click(sender As Object, e As EventArgs) Handles BtnBack.Click
-        RunInUi(Sub()
-                    TextServer.Text = Nothing
-                    TextName.Text = Nothing
-                    TextPass.Password = Nothing
-                    FrmLaunchLeft.RefreshPage(True)
-                End Sub)
+        TextServer.Text = Nothing
+        TextName.Text = Nothing
+        TextPass.Password = Nothing
+        FrmLaunchLeft.RefreshPage(True)
     End Sub
-    Private Shared ReadOnly ValidateHttp As New ValidateHttp()
+    Private Shared ReadOnly PatternHttpUri As New Regex("^(http[s]?)\://")
     Private Sub BtnLogin_Click(sender As Object, e As EventArgs) Handles BtnLogin.Click
-        If Not ValidateHttp.Validate(TextServer.Text) Then 
-            Hint("输入的验证服务器地址无效", HintType.Critical)
-            Exit Sub
-        End If
         If String.IsNullOrWhiteSpace(TextServer.Text) OrElse String.IsNullOrWhiteSpace(TextName.Text) OrElse String.IsNullOrWhiteSpace(TextPass.Password) Then
             Hint("验证服务器、用户名与密码均不能为空！", HintType.Critical)
+            Exit Sub
+        End If
+        If Not PatternHttpUri.IsMatch(TextServer.Text) Then
+            Hint("输入的验证服务器地址无效", HintType.Critical)
             Exit Sub
         End If
         BtnLogin.IsEnabled = False
         BtnBack.IsEnabled = False
         Dim LoginData As New McLoginServer(McLoginType.Auth) With {.BaseUrl = If(TextServer.Text.EndsWithF("/"),
             TextServer.Text & "authserver", TextServer.Text & "/authserver"), .UserName = TextName.Text, .Password = TextPass.Password, .Description = "Authlib-Injector", .Type = McLoginType.Auth}
-        RunInNewThread(Sub()
-                           Try
-                               IsCreatingProfile = True
-                               McLoginAuthLoader.Start(LoginData, IsForceRestart:=True)
-                               Do While McLoginAuthLoader.State = LoadState.Loading
-                                   RunInUi(Sub() BtnLogin.Text = Math.Round(McLoginAuthLoader.Progress * 100) & "%")
-                                   Thread.Sleep(50)
-                               Loop
-                               If McLoginAuthLoader.State = LoadState.Finished Then
-                                   RunInUi(Sub() FrmLaunchLeft.RefreshPage(True))
-                               ElseIf McLoginAuthLoader.State = LoadState.Aborted Then
-                                   Throw New ThreadInterruptedException
-                               ElseIf McLoginAuthLoader.Error Is Nothing Then
-                                   Throw New Exception("未知错误！")
-                               Else
-                                   Throw New Exception(McLoginAuthLoader.Error.Message, McLoginAuthLoader.Error)
-                               End If
-                           Catch ex As ThreadInterruptedException
-                               Hint("已取消登录！")
-                           Catch ex As Exception
-                               If ex.Message = "$$" Then
-                               ElseIf ex.Message.StartsWith("$") Then
-                                   Hint(ex.Message.TrimStart("$"), HintType.Critical)
-                               Else
-                                   Log(ex, "第三方登录尝试失败", LogLevel.Msgbox)
-                               End If
-                           Finally
-                               RunInUi(
-                               Sub()
-                                   IsCreatingProfile = False
-                                   BtnLogin.IsEnabled = True
-                                   BtnBack.IsEnabled = True
-                                   BtnLogin.Text = "登录"
-                               End Sub)
-                           End Try
-                       End Sub)
+        Dispatcher.BeginInvoke(Async Function() As Task
+            Try
+                IsCreatingProfile = True
+                McLoginAuthLoader.Start(LoginData, IsForceRestart:=True)
+                Do While McLoginAuthLoader.State = LoadState.Loading
+                    BtnLogin.Text = Math.Round(McLoginAuthLoader.Progress * 100) & "%"
+                    Await Task.Delay(50)
+                Loop
+                If McLoginAuthLoader.State = LoadState.Finished Then
+                    FrmLaunchLeft.RefreshPage(True)
+                ElseIf McLoginAuthLoader.State = LoadState.Aborted Then
+                    Hint("已取消登录！")
+                ElseIf McLoginAuthLoader.Error Is Nothing Then
+                    Throw New Exception("未知错误！")
+                Else
+                    Throw New Exception(McLoginAuthLoader.Error.Message, McLoginAuthLoader.Error)
+                End If
+            Catch ex As Exception
+                If ex.Message = "$$" Then
+                ElseIf ex.Message.StartsWith("$") Then
+                    Hint(ex.Message.TrimStart("$"), HintType.Critical)
+                Else
+                    Log(ex, "第三方登录尝试失败", LogLevel.Msgbox)
+                End If
+            Finally
+                IsCreatingProfile = False
+                BtnLogin.IsEnabled = True
+                BtnBack.IsEnabled = True
+                BtnLogin.Text = "登录"
+            End Try
+        End Function)
     End Sub
     '获取验证服务器名称
     Private Sub GetServerName() Handles TextServer.LostKeyboardFocus
         Dim serverUriInput = TextServer.Text
-        If String.IsNullOrWhiteSpace(serverUriInput) Then Exit Sub
-        RunInNewThread(Sub()
+        If String.IsNullOrWhiteSpace(serverUriInput) Then
+            TextServerName.Visibility = Visibility.Hidden
+            Exit Sub
+        End If
+        Dispatcher.BeginInvoke(Async Function() As Task
             Dim serverUri As String = Nothing
             Dim serverName As String = Nothing
             Try
-                serverUri = ApiLocation.TryRequest(serverUriInput).GetAwaiter().GetResult()
-                Dim response As String = NetGetCodeByRequestRetry(serverUri, Encoding.UTF8)
-                serverName = JObject.Parse(response)("meta")("serverName").ToString()
+                serverUri = Await ApiLocation.TryRequest(serverUriInput)
+                Dim response = Await HttpRequestBuilder.Create(serverUri, HttpMethod.Get).SendAsync()
+                Dim responseText As String = Await response.AsStringAsync()
+                serverName = Await Task.Run(Function() JObject.Parse(responseText)("meta")("serverName").ToString())
             Catch ex As Exception
                 Log(ex, "从服务器获取名称失败", LogLevel.Debug)
             End Try
-            RunInUi(Sub()
-                If serverUri IsNot Nothing Then TextServer.Text = serverUri
-                If serverName Is Nothing Then
-                    TextServerName.Visibility = Visibility.Hidden
-                Else
-                    TextServerName.Text = "验证服务器: " & serverName
-                    TextServerName.Visibility = Visibility.Visible
-                End If
-            End Sub)
-        End Sub)
+            If serverUri IsNot Nothing Then TextServer.Text = serverUri
+            If serverName Is Nothing Then
+                TextServerName.Visibility = Visibility.Hidden
+            Else
+                TextServerName.Text = "验证服务器: " & serverName
+                TextServerName.Visibility = Visibility.Visible
+            End If
+        End Function)
     End Sub
     '链接处理
     Private Sub ComboName_TextChanged() Handles TextName.TextChanged
