22ca8a9a78833e8e07ca2817176f7665d39fe3ab
任天天 <61044187+ruattd@users.noreply.github.com>
2025-08-28 11:10:26 +0800

    fix(ui): 修复 Tablet 设备默认实现失效 (#1467)

--- Plain Craft Launcher 2/Program.vb
+++ Plain Craft Launcher 2/Program.vb
@@ -21,6 +21,9 @@ Module Program
                 form.InitializeComponent()
                 Return form
             End Function
+        'From dotnet/wpf #2393: fix tablet devices broken on .NET Core 3.0+
+        'ReSharper disable once UnusedVariable
+        Dim vbSucks = Tablet.TabletDevices
         'Start lifecycle
         Lifecycle.OnInitialize()
     End Sub
